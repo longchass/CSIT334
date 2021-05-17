c4c5d9d5d26ec65d63e5c5544f9a8f1901cb20eb
BaoLam <ngblam2207@gmail.com>
2021-05-17 06:08:44 -0500

    xa

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< Updated upstream
 	$str_browser_language = !empty($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? strtok(strip_tags($_SERVER['HTTP_ACCEPT_LANGUAGE']), ',') : '';
 	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
 	switch (substr($str_browser_language, 0,2))
@@ -43,6 +44,134 @@
         margin: 0;
         padding: 10px 20px 20px 20px;
     }
+=======
+
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$sql = $priv = "";
+$username_err = $password_err = $login_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+	
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+		$priv = "SELECT id, username, privs FROM USERS WHERE username = ?";
+		
+		if($stmt = mysqli_prepare($link, $priv)) {
+			// Bind variables to the prepared statement as parameters
+			mysqli_stmt_bind_param($stmt, "s", $param_username);
+			
+			// Set parameters
+			$param_username = $username;
+			
+			// Attempt to execute the prepared statement
+			if(mysqli_stmt_execute($stmt)){
+				// Store result
+				mysqli_stmt_store_result($stmt);
+					
+				// Check if username exists, if yes then find their privilege
+				if(mysqli_stmt_num_rows($stmt) == 1){   
+					mysqli_stmt_bind_result($stmt, $id, $username, $privs);
+					if($privs == '#P') {
+						$sql = "SELECT id, username, password FROM person WHERE username = ?";
+					} elseif ($privs == '#S') {
+						$sql = "SELECT id, username, password FROM staff WHERE username = ?";
+					}
+					verify_password($sql);
+				}
+			} else{
+				echo "Oops! Username does not exist. Please try again.";
+			}
+			mysqli_stmt_close($stmt);
+		}
+    }
+	
+	function verify_password($sql) {
+		if($stmt = mysqli_prepare($link, $sql)){
+			// Bind variables to the prepared statement as parameters
+			mysqli_stmt_bind_param($stmt, "s", $param_username);
+			
+			// Set parameters
+			$param_username = $username;
+			
+			// Attempt to execute the prepared statement
+			if(mysqli_stmt_execute($stmt)){
+				// Store result
+				mysqli_stmt_store_result($stmt);
+					
+				// Check if username exists, if yes then verify password
+				if(mysqli_stmt_num_rows($stmt) == 1){                    
+					// Bind result variables
+					mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+					if(mysqli_stmt_fetch($stmt)){
+						if(password_verify($password, $hashed_password)){
+							// Password is correct, so start a new session
+							session_start();
+								
+							// Store data in session variables
+							$_SESSION["loggedin"] = true;
+							$_SESSION["id"] = $id;
+							$_SESSION["username"] = $username;                          
+							$_SESSION["priv"] =$privs
+							// Redirect user to welcome page
+							header("location: welcome.php");
+						} else{
+							// Password is not valid, display a generic error message
+							$login_err = "Invalid username or password.";
+						}
+					}
+				} else{
+					// Username doesn't exist, display a generic error message
+					$login_err = "Invalid username or password.";
+				}
+			} else{
+				echo "Oops! Something went wrong. Please try again later.";
+			}
+
+			// Close statement
+			mysqli_stmt_close($stmt);
+		}
+	}
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+>>>>>>> Stashed changes
 
     samp {
         font-size: 1.3em;
@@ -83,6 +212,7 @@
 
 <?php if ($str_language == 'de'): ?>
 
+<<<<<<< Updated upstream
     <p class="text"><strong>Der virtuelle <span lang="en" xml:lang="en">Host</span> wurde erfolgreich eingerichtet.</strong></p>
     <p class="text">Wenn Sie diese Seite sehen, dann bedeutet dies, dass der neue virtuelle <span lang="en" xml:lang="en">Host</span> erfolgreich eingerichtet wurde. Sie können jetzt Ihren <span lang="en" xml:lang="en">Web</span>-Inhalt hinzufügen, diese Platzhalter-Seite<sup><a href="#footnote_1">1</a></sup> sollten Sie ersetzen <abbr title="beziehungsweise">bzw.</abbr> löschen.</p>
     <p class="text">
@@ -92,6 +222,30 @@
     <p class="text" id="footnote_1"><small><sup>1</sup> Dateien: <samp>index.php</samp> und <samp>MAMP-PRO-Logo.png</samp></small></p>
     <hr />
     <p class="text">This page in: <?php echo $str_available_languages; ?></p>
+=======
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            
+                
+                <input type="text" name="username" placeholder="Username" required="required <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+              
+            
+               
+                <input type="password" name="password" placeholder="Password" required="required" <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            
+            
+                <input type="submit" class="btn btn-primary btn-block btn-large" value="Login">
+            
+            <p>Sign Up: <a href="registerPersonal.php">For myself</a> or <a href="registerBusiness.php">For my business</a></p>
+        </form>
+		
+		
+    </div>
+    </div>
+</body>
+</html>
+>>>>>>> Stashed changes
 
 <?php elseif ($str_language == 'en'): ?>
 
---
+++ standardcreate.sql
@@ -0,0 +1,85 @@
+CREATE TABLE USERS (
+	ID		INT(8)		NOT NULL AUTO_INCREMENT,
+	username	VARCHAR(50)		NOT NULL UNIQUE,
+	privs	CHAR(2)		NOT NULL,
+	
+	CONSTRAINT USER_PKEY PRIMARY KEY (ID, username)
+);
+#user account table
+CREATE TABLE person (
+	ID		INT(8)		NOT NULL AUTO_INCREMENT,
+	username	VARCHAR(50)		NOT NULL UNIQUE,
+    password	VARCHAR(50) NOT NULL,
+	fname VARCHAR(25) NOT NULL, 		
+	lname VARCHAR(25) NOT NULL, 		
+	email VARCHAR(150) NOT NULL,
+	phone CHAR(10) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (ID, username), 
+	FOREIGN KEY (ID, username) REFERENCES USERS(ID, username)
+);
+
+CREATE TABLE business (
+	ID		INT(8)		NOT NULL AUTO_INCREMENT,
+	username	VARCHAR(50)		NOT NULL UNIQUE,
+    name	VARCHAR(30)	NOT NULL,
+	password	VARCHAR(50) NOT NULL,
+	email	VARCHAR(50)	NOT NULL,
+	address	VARCHAR(150)	NOT NULL,
+	phone	CHAR(10)	NOT NULL,
+	PRIMARY KEY (ID, username), 
+	FOREIGN KEY (ID, username) REFERENCES USERS(ID, username)
+);
+
+#table for all vaccine certificate
+CREATE TABLE VACCINE_CERT (
+    ID			INT(8) 	NOT NULL,		/*unique user ID*/
+	P_ID		INT(8)		NOT NULL,
+	name 		VARCHAR(50) NOT NULL,		/*the user vaccinated*/
+    vac_date 	DATE	 NOT NULL,			/*date/time user vaccinated - change to just date?*/
+	
+	CONSTRAINT PERSON_FKEY FOREIGN KEY (P_ID) REFERENCES person (ID)
+);
+
+#table for admin health staff
+CREATE TABLE STAFF (
+	ID		INT(8)		NOT NULL,		/*unique user ID*/
+	privs	CHAR(2)		NOT NULL,
+	username	VARCHAR(50)	NOT NULL UNIQUE,
+    password	VARCHAR(50) NOT NULL,
+	fname	VARCHAR(25) NOT NULL,
+	lname	VARCHAR(25) NOT NULL,
+	department	VARCHAR(40)		NOT NULL,
+	
+	CONSTRAINT STAFF_FKEY FOREIGN KEY (ID, username) REFERENCES USERS(ID, username)
+);
+
+CREATE TABLE ADMIN (
+	ID		INT(8)		NOT NULL,		/*unique admin ID for government*/
+	privs	CHAR(2)		NOT NULL,
+	username	VARCHAR(50)	NOT NULL UNIQUE,
+    password	VARCHAR(50) NOT NULL,
+	fname	VARCHAR(25) NOT NULL,
+	lname	VARCHAR(25) NOT NULL,
+	department	VARCHAR(40)		NOT NULL,
+	
+	CONSTRAINT ADMIN_PKEY PRIMARY KEY (ID)
+);
+
+INSERT INTO ADMIN VALUES(0, '#A','baolam', 'admin', 'bao', 'lam', 'Australia Health Department')
+
+#table for check-in logs
+CREATE TABLE CHECKIN (
+	ID		INT(12)	NOT NULL AUTO_INCREMENT,		/*unique ID*/
+	P_ID	INT(8)		NOT NULL,		/*Personal ID*/
+    name	VARCHAR(50) NOT NULL,		
+	B_ID	INT(8)		NOT NULL,		/*Business ID*/
+	address VARCHAR(50) NOT NULL,	
+	check_in_time	DATETIME DEFAULT CURRENT_TIMESTAMP,	/*Current time used to sign in*/
+	check_out_time	DATETIME,							/*Time signed out*/
+	
+	CONSTRAINT CHECKIN_PKEY PRIMARY KEY(ID),
+	CONSTRAINT P_FK FOREIGN KEY (P_ID) REFERENCES person (ID), 		/*field has to exist in person table*/
+	CONSTRAINT B_FK FOREIGN KEY (B_ID) REFERENCES business (ID)		/*field has to exist in business table*/
+);
+
