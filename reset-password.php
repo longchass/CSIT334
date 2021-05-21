8e243f268c101afcd8839314eed2fb4eeef3dfdc
BaoLam <ngblam2207@gmail.com>
2021-05-21 02:44:08 -0500

    Update reset-password.php
    update reset password to work just fine and redirect to welcome page without destroying the session

--- reset-password.php
+++ reset-password.php
@@ -40,24 +40,44 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Check input errors before updating the database
     if(empty($new_password_err) && empty($confirm_password_err)){
         // Prepare an update statement
-        $sql = "UPDATE users SET password = ? WHERE id = ?";
+		if($_SESSION["privs"] == "#P") {
+			$sql = "UPDATE person SET password = ? WHERE username = ?";
+		} elseif($_SESSION["privs"] == "#S") {
+			$sql = "UPDATE staff SET password = ? WHERE username = ?";
+		} elseif($_SESSION["privs"] == "#A") {
+			$sql = "UPDATE admin SET password = ? WHERE username = ?";
+		} elseif($_SESSION["privs"] == "#B") {
+			$sql = "UPDATE business SET password = ? WHERE username = ?";
+		}
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_id);
+            mysqli_stmt_bind_param($stmt, "ss", $param_password, $param_username);
             
             // Set parameters
-            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
-            $param_id = $_SESSION["id"];
+            //$param_password = password_hash($new_password, PASSWORD_DEFAULT);
+			$param_password = $new_password;
+            $param_username = $_SESSION["username"];
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                // Password updated successfully. Destroy the session, and redirect to login page
-                session_destroy();
-                header("location: index.php");
-                exit();
+                //Do not destroy the session, and redirect to welcome page
+                //session_destroy();
+				
+				//Re-redirect to welcome page
+				if($_SESSION["privs"] == "#P") {
+					header("location:welcome.php");
+				} elseif($_SESSION["privs"] == "#S") {
+					header("location:welcomestaff.php");
+				} elseif($_SESSION["privs"] == "#A") {
+					header("location:welcomeadmin.php");
+				} elseif($_SESSION["privs"] == "#B") {
+					header("location:welcomebusiness.php");
+				}
+				
+                //exit();
             } else{
-                echo "Oops! Something went wrong. Please try again later.";
+                echo "Failed to reset password";
             }
 
             // Close statement
@@ -77,8 +97,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Reset Password</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
+        .wrapper{ width: 350px; padding: 20px; margin:auto }
+		    <style>
+		* { -webkit-box-sizing:border-box; -moz-box-sizing:border-box; -ms-box-sizing:border-box; -o-box-sizing:border-box; box-sizing:border-box; }
+
+		html { width: 100%; height:100%; overflow:hidden; }
+		
+        body { 
+		  color: #fff;
+		  text-align: center; 
+		  width: 100%;
+		  height:100%;
+		  font-family: 'Open Sans', sans-serif;
+		  background: #092756;
+		  background: -moz-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%),-moz-linear-gradient(top,  rgba(57,173,219,.25) 0%, rgba(42,60,87,.4) 100%), -moz-linear-gradient(-45deg,  #670d10 0%, #092756 100%);
+		  background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -webkit-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -webkit-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+		  background: -o-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -o-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -o-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+		  background: -ms-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), -ms-linear-gradient(top,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), -ms-linear-gradient(-45deg,  #670d10 0%,#092756 100%);
+		  background: -webkit-radial-gradient(0% 100%, ellipse cover, rgba(104,128,138,.4) 10%,rgba(138,114,76,0) 40%), linear-gradient(to bottom,  rgba(57,173,219,.25) 0%,rgba(42,60,87,.4) 100%), linear-gradient(135deg,  #670d10 0%,#092756 100%);
+		  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3E1D6D', endColorstr='#092756',GradientType=1 );
+			}
+			.login { 
+
+}
     </style>
 </head>
 <body>
