b790a06943eb23340a9da21741c87ef7cab8afe3
BaoLam <ngblam2207@gmail.com>
2021-05-25 21:27:07 -0500

    Update Staff.php
    typo

--- classes/Staff.php
+++ classes/Staff.php
@@ -1,7 +1,6 @@
 <?php
 //require vaccine_cert.php;
-require 'Peson.php';
-require '{parent_dir}config.php';
+require 'person.php';
 
 class Staff {
   // Properties
@@ -19,62 +18,6 @@ class Staff {
 	public function vaccinated($username, $name, $vac_date)
 	{
 		$vaccine_cert = new vaccine_cert($username, $name, $vac_date);
-	}
-	function displayAllUser() {
-	$sql = 'SELECT PERSON.USERNAME, FNAME, LNAME, INFECTED, VACCINE_TYPE
-			FROM PERSON LEFT OUTER JOIN vaccine_cert
-			ON person.username = vaccine_cert.username';
-		if($stmt = $link->query($sql) )
-		{
-			echo "<div id='header' ></div>";
-			echo "<h3>Profile information</h3>";
-			echo "<table width='80%' border='1' align='center'>\n";
-			echo "<tr><th>Username</th><th>First name</th><th>Last name</th><th>Infected</th></tr>";
-			while($row = $stmt->fetch_row() )
-			{
-				if($row[3] <> 1) $row[3] = 0;
-				$Person = new $Person($row[0],$row[1],$row[2]);
-				$Person->set_infected($row[3]);
-				echo "<tr><td>".$Person->get_username()."</td>\n";
-				echo "<td>".$Person->get_fname()."</td>\n";
-				echo "<td>".$Person->get_lname()."</td>\n";
-				echo "<td>".$Person->get_infected()."</td>\n";
-				echo "<td>"$row[4]"</td></tr>\n";	
-			}
-			echo "</table>\n";
-			$stmt->close();
-		}
-	}
-
-	function findUser(string $u) {
-		$sql = "SELECT PERSON.USERNAME, FNAME, LNAME, INFECTED, VACCINE_TYPE
-				FROM person LEFT OUTER JOIN vaccine_cert
-				ON person.username = vaccine_cert.username
-				where person.username like ?";
-		if($stmt = $link->prepare($sql) ) {
-			$stmt->bind_param("s",$_GET['name']);
-			
-			if($stmt->execute() ) {
-				echo "<div id='header' ></div>";
-				echo "<h3>Profile information</h3>";
-				echo "<table width='80%' border='1' align='center'>\n";
-				echo "<tr><th>Username</th><th>First name</th><th>Last name</th><th>Infected</th><th>Vaccine</th></tr>";
-				$result = $stmt->get_result();
-				while($row = $result->fetch_row() ) {
-					if ($row[3] <> 1) $row[3] = 0;
-				
-					$Person = new Person($row[0],$row[1],$row[2]);
-					$Person->set_infected($row[3]);
-					echo "<tr><td>".$Person->get_username()."</td>\n";
-					echo "<td>".$Person->get_fname()."</td>\n";
-					echo "<td>".$Person->get_lname()."</td>\n";
-					echo "<td>".$Person->get_infected()."</td>\n";
-					echo "<td>$row[4]</td></tr>\n";	
-				}
-				echo "</table>\n";
-				$stmt->close();
-			}
-		}
 	}
   public function set_fname($fname)
   {
