24c139dec881b35ca2eeb0c77d9c63eb7b0fc13f
BaoLam <ngblam2207@gmail.com>
2021-05-27 02:37:37 -0500

    Update standardload.sql
    new load

--- standardload.sql
+++ standardload.sql
@@ -68,48 +68,48 @@ INSERT INTO BUSINESS VALUES('business4', 'password', 'bname', 'address4',0 , 100
 INSERT INTO USERS VALUES('business5', '#B');
 INSERT INTO BUSINESS VALUES('business5', 'password', 'bname', 'address5', 0, 50);
 
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person1', 'fname lname', 'business1', 'address1',TIMESTAMP('2021-04-02 10:35:12'),TIMESTAMP('2021-04-02 11:35:12') );
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person2', 'fname lname', 'business3', 'address3',TIMESTAMP('2021-04-02 10:30:12'),TIMESTAMP('2021-04-02 14:35:12'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person3', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-02 11:20:12'),TIMESTAMP('2021-04-02 12:00:12'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person3', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-02 15:35:12'),TIMESTAMP('2021-04-02 15:45:12'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person6', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-02 18:35:12'),TIMESTAMP('2021-04-02 18:55:10'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person4', 'fname lname', 'business3', 'address3',TIMESTAMP('2021-04-02 18:45:12'),TIMESTAMP('2021-04-02 18:55:01'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person6', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-02 18:51:13'),TIMESTAMP('2021-04-02 18:56:32'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person5', 'fname lname', 'business1', 'address1',TIMESTAMP('2021-04-02 18:53:01'),TIMESTAMP('2021-04-02 18:59:14'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person7', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-02 19:05:12'),TIMESTAMP('2021-04-02 19:35:12'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person8', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-03 19:45:10'),TIMESTAMP('2021-04-03 20:40:11'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person4', 'fname lname', 'business3', 'address3',TIMESTAMP('2021-04-03 18:45:12'),TIMESTAMP('2021-04-03 18:55:01'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person3', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-03 18:51:13'),TIMESTAMP('2021-04-03 18:56:32'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person5', 'fname lname', 'business1', 'address1',TIMESTAMP('2021-04-03 18:53:01'),TIMESTAMP('2021-04-03 18:59:14'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person9', 'fname lname', 'business5', 'address5',TIMESTAMP('2021-04-03 19:05:12'),TIMESTAMP('2021-04-03 19:35:12'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person8', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-03 19:45:10'),TIMESTAMP('2021-04-03 19:35:12'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person1', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-03 19:45:10'),TIMESTAMP('2021-04-03 20:40:11'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person4', 'fname lname', 'business3', 'address3',TIMESTAMP('2021-04-04 18:45:12'),TIMESTAMP('2021-04-04 18:55:01'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person3', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-04 18:51:13'),TIMESTAMP('2021-04-04 18:56:32'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person5', 'fname lname', 'business1', 'address1',TIMESTAMP('2021-04-04 18:53:01'),TIMESTAMP('2021-04-04 18:59:14'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person2', 'fname lname', 'business5', 'address5',TIMESTAMP('2021-04-04 19:05:12'),TIMESTAMP('2021-04-04 19:35:12'));
-INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out)
-VALUES('person1', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-04 19:45:10'),TIMESTAMP('2021-04-04 19:35:12'));
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person1', 'fname lname', 'business1', 'address1',TIMESTAMP('2021-04-02 10:35:12'),TIMESTAMP('2021-04-02 11:35:12'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person2', 'fname lname', 'business3', 'address3',TIMESTAMP('2021-04-02 10:30:12'),TIMESTAMP('2021-04-02 14:35:12'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person3', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-02 11:20:12'),TIMESTAMP('2021-04-02 12:00:12'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person3', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-02 15:35:12'),TIMESTAMP('2021-04-02 15:45:12'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person6', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-02 18:35:12'),TIMESTAMP('2021-04-02 18:55:10'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person4', 'fname lname', 'business3', 'address3',TIMESTAMP('2021-04-02 18:45:12'),TIMESTAMP('2021-04-02 18:55:01'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person6', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-02 18:51:13'),TIMESTAMP('2021-04-02 18:56:32'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person5', 'fname lname', 'business1', 'address1',TIMESTAMP('2021-04-02 18:53:01'),TIMESTAMP('2021-04-02 18:59:14'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person7', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-02 19:05:12'),TIMESTAMP('2021-04-02 19:35:12'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person8', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-03 19:45:10'),TIMESTAMP('2021-04-03 20:40:11'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person4', 'fname lname', 'business3', 'address3',TIMESTAMP('2021-04-03 18:45:12'),TIMESTAMP('2021-04-03 18:55:01'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person3', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-03 18:51:13'),TIMESTAMP('2021-04-03 18:56:32'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person5', 'fname lname', 'business1', 'address1',TIMESTAMP('2021-04-03 18:53:01'),TIMESTAMP('2021-04-03 18:59:14'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person9', 'fname lname', 'business5', 'address5',TIMESTAMP('2021-04-03 19:05:12'),TIMESTAMP('2021-04-03 19:35:12'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person8', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-03 19:45:10'),TIMESTAMP('2021-04-03 19:35:12'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person1', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-03 19:45:10'),TIMESTAMP('2021-04-03 20:40:11'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person4', 'fname lname', 'business3', 'address3',TIMESTAMP('2021-04-04 18:45:12'),TIMESTAMP('2021-04-04 18:55:01'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person3', 'fname lname', 'business4', 'address4',TIMESTAMP('2021-04-04 18:51:13'),TIMESTAMP('2021-04-04 18:56:32'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person5', 'fname lname', 'business1', 'address1',TIMESTAMP('2021-04-04 18:53:01'),TIMESTAMP('2021-04-04 18:59:14'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person2', 'fname lname', 'business5', 'address5',TIMESTAMP('2021-04-04 19:05:12'),TIMESTAMP('2021-04-04 19:35:12'),false,false);
+INSERT INTO CHECKIN (p_username, name, b_username, address, check_in, check_out, infected, pos_contact)
+VALUES('person1', 'fname lname', 'business2', 'address2',TIMESTAMP('2021-04-04 19:45:10'),TIMESTAMP('2021-04-04 19:35:12'),false,false);
 
 INSERT INTO VACCINE_CERT VALUES('person10', 'Pfizer', DATE('2021-04-01'), 'staff1');
 INSERT INTO VACCINE_CERT VALUES('person5', 'AstraZeneca', DATE('2021-04-10'), 'staff3' );
